5a6a37482101d50f55f7b5e78643ec4056a1bb7d
gabriel_fagundes_souza <gabriel.fagundes.souza.2209@gmail.com>
2026-06-24 11:28:08 -0300

    Add files via upload

---
+++ atividade 1/atividade 1/bin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace atividade_1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void Btn_verificarClick(object sender, EventArgs e)
+        	
+        {
+        	int numero;
+            // Converte o texto para inteiro
+            if (int.TryParse(txt_num.Text, out numero))
+            {
+                // Verifica se é par ou ímpar
+                if (numero % 2 == 0)
+                {
+                	lbl_result.Text = "O número  é PAR.";
+                }
+                else
+                {
+                    lbl_result.Text = "O número é ÍMPAR.";
+                 
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Por favor, digite um número inteiro válido.",
+                    "Erro de entrada",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                txt_num.Clear();
+                txt_num.Focus();
+            }
+        }
+    }
+}
